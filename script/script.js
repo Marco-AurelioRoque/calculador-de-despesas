f14119d9686571a7a54160cd01400c981fd9f93f
Marco-AurelioRoque <marcoaureliocraft@gmail.com>
2021-04-18 23:40:52 -0300

    sistema de tabela - failed

--- script/script.js
+++ script/script.js
@@ -15,11 +15,39 @@ function adiciona() {
     let receita = prompt("Receita: ")
     let valor = prompt("Valor: ")
 
-    
-}
 
+}
 
 
 function limpa() {
 
 }
+
+//Criando elementos para o tbody
+
+var corpoTabela = document.querySelector("tbody")
+
+
+this.criarLinhaTabela = function (){
+    var linha = document.createElement("tr")
+    var Receita = document.createElement("td")
+    var Previsto = document.createElement("td")
+    var Ganho = document.createElement("td")
+
+    var textoReceita = document.createTextNode(receita)
+    var textoPrevisto = document.createTextNode(previsto)
+    var textoGanho = document.createTextNode(valor)
+
+    Receita.appendChild(textoReceita)
+    Previsto.appendChild(textoPrevisto)
+    Ganho.appendChild(textoGanho)
+
+    linha.appendChild(Receita)
+    linha.appendChild(Previsto)
+    linha.appendChild(Ganho)
+
+    corpoTabela.appendChild(linha)
+
+}
+
+criarLinhaTabela()

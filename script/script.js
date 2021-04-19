e098a748ea7a17b43d205d4db53d0850413290cb
Marco-AurelioRoque <marcoaureliocraft@gmail.com>
2021-04-18 23:03:35 -0300

    html e css finalizado

---
+++ script/script.js
@@ -0,0 +1,25 @@
+//Abre fecha menu
+
+function menuToggle() {
+    let menuArea = document.getElementById("menu-area")
+
+    if(menuArea.style.width == "200px") {
+        menuArea.style.width = "0px"
+    } else{
+        menuArea.style.width = "200px"
+    }
+}
+
+//Botoes adiciona, limpa
+function adiciona() {
+    let receita = prompt("Receita: ")
+    let valor = prompt("Valor: ")
+
+    
+}
+
+
+
+function limpa() {
+
+}
